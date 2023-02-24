125a1b4af48d51055e69b3fbac40e263a0c68815
KolosRoman <romakolos84@gmail.com>
2023-02-24 14:48:34 +0200

    Add index file and script file

---
+++ script.js
@@ -0,0 +1,6 @@
+alert('Hello');
+let userName = prompt('What is your name?');
+let userAge = prompt('What is your birth year?');
+let userCountry = prompt('Where are you from?');
+
+alert('Nice to meet you, ' + userName + '!' + ' You are ' + (2023 - +userAge) + ' years old! You are from ' + userCountry + '!');
